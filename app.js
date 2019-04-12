11e66ad841a5dd0da0254a4f765f5ba3ad1b5d28
Taylor Harvey <larothion@aol.com>
2019-04-12 17:34:36 -0600

    click and mortar landing page v1

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("app is listening...");
+});
